609e06749304562d5bb9c3d22c02bbfb5ebddc98
EdgarAlvarado24 <edgar.alvarado.16@gmail.com>
2026-02-02 15:06:52 -0400

    implement key in api and development app

--- src/services/rates.service.ts
+++ src/services/rates.service.ts
@@ -2,6 +2,10 @@ import axios from "axios";
 import { RatesData } from "../types/rates";
 
 export async function getRates(): Promise<RatesData> {
-  const response = await axios.get("https://api.dolarvzla.com/public/exchange-rate");
+  const response = await axios.get("https://api.dolarvzla.com/public/exchange-rate", {
+    headers: {
+      "x-dolarvzla-key": "21a2bc0070e3e2df9312fa8199b2ab9f5debf9ade8d8bd9a1119f805e5aff8a2"
+    }
+  });
   return response.data;
 }
